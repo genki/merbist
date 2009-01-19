b259eece877775e3f397500bd778ce529d6f1419
takiuchi <genki@s21g.com>
2009-01-19 12:02:08 +0900

    Changed dir of gems.

--- app/controllers/gems.rb
+++ app/controllers/gems.rb
@@ -1,7 +1,7 @@
 class Gems < Application
   def show(name)
     only_provides :gem
-    gem_path = File.join(Merb::Config[:gem_home], 'gems', name)
+    gem_path = File.join(Merb::Config[:gem_home], 'cache', name)
     send_file gem_path
   end
 end
