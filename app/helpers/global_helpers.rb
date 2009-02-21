3c032fb05a5f0c2b657e343349b52844705c2a5f
takiuchi <genki@s21g.com>
2009-02-22 03:59:18 +0900

    Added gavater to user's page.

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,5 +1,10 @@
 module Merb
   module GlobalHelpers
     # helpers defined here available to all views.  
+    def gravater(email, size = 80)
+      id = Digest::MD5.hexdigest(email)
+      uri = "http://www.gravatar.com/avatar/#{id}?s=#{size}"
+      tag :img, nil, :src => uri, :width => size, :height => size, :border => 0
+    end
   end
 end
