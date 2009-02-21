0d9125780eacc4304f91d2c408f3c62a215f4d4c
takiuchi <genki@s21g.com>
2009-02-22 04:20:31 +0900

    Added avatar images.

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,10 +1,11 @@
 module Merb
   module GlobalHelpers
     # helpers defined here available to all views.  
-    def gravater(email, size = 80)
+    def gravater(email, size = 80, attrs = {})
       id = Digest::MD5.hexdigest(email)
       uri = "http://www.gravatar.com/avatar/#{id}?s=#{size}"
-      tag :img, nil, :src => uri, :width => size, :height => size, :border => 0
+      tag :img, nil, {:src => uri, :width => size,
+        :height => size, :border => 0}.merge(attrs)
     end
   end
 end
